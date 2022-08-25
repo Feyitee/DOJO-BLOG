6540b33e5c5c1854690ad0335dc1f0c44da2f70c
Feyi <anjuwontobi99@gamil.com>
2022-08-26 00:05:04 +0100

    Controlled Inputs, Submit Events and Post Request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 const App = () => {
   return (
@@ -10,18 +11,20 @@ const App = () => {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
             <Route path="/create">
-              <Home />
+              <Create />
             </Route>
-            <Create />
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
           </Switch>
         </div>
       </div>
     </Router>
   );
 };
-
+3;
 export default App;
